a9d157ce3abf3de70b26d3206aae2362bc604fc3
Shahzod <shhayitboyevich@gmail.com>
2023-10-09 00:39:51 +0500

    remove_web

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $accuWeather = Http::get('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=LFkfsNOeb6uUrbrVEey3kHHYcISY1Xyd&q=tashkent');
-    $json = json_decode($accuWeather->getBody());
-    $key = $json[0]->Key;
-
-    $getWeather = Http::get('http://dataservice.accuweather.com/currentconditions/v1/'.$key.'?apikey=LFkfsNOeb6uUrbrVEey3kHHYcISY1Xyd');
-    $data = json_decode($getWeather->getBody());
-    dd($data[0]->Temperature->Metric->Value);
-//    $openWeather = Http::get('https://api.openweathermap.org/data/2.5/weather?q=tashkent&appid=d7f030e1b1b35529aee8504dbc900ed3&units=metric');
-//    $json = json_decode($openWeather->getBody());
-
+    return view('welcome');
 });
